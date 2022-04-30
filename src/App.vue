27f03e2d875ab9c02e8a8fa9e2455d7cb27aa848
xiongsi <815773075@qq.com>
2022-04-30 20:40:26 +0800

    vant rem 适配

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,17 @@
   <div id="app">
 
     <h1>黑马头条</h1>
+    <!-- 测试 iconfont -->
+    <!-- <i class="toutiao toutiao-dianzan"></i> -->
+
+    <!-- 测试vant -->
+    <!-- <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button> -->
     <!-- 路由出口 -->
+    
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 import store from './store'
 import './styles/index.less'
 
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import 'amfe-flexible'
+
+
+Vue.use(Vant);
+
 Vue.config.productionTip = false
 
 new Vue({
